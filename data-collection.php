81385e04743f99bdb12cedf908afb4218031522d
Your Name <you@example.com>
2024-06-22 09:12:00 +0530

    Changes made by udit updated new images and enhance index.php

---
+++ data-collection.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+  <?php
+  $page_title = "Data Collection";
+  $page_keywords = "home, data entry, services";
+  $page_description = "Welcome to the Home Page of Data Entry Expert, offering the best data entry services.";
+  ?>
+  <?php include("includes/header.php"); ?>
+
+</head>
+
+<body>
+  <!-- Spinner Start -->
+  <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+    <div class="spinner-grow text-primary" style="width: 3rem; height: 3rem" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>
+  <!-- Spinner End -->
+
+  <!-- Navbar Start -->
+  <?php include("includes/navbar.php"); ?>
+  <!-- Navbar End -->
+
+  <!-- Hero Start -->
+  <div class="container-fluid pt-5 bg-primary hero-header">
+    <div class="container pt-5">
+      <div class="row g-5 pt-5">
+        <div class="col-lg-6 align-self-center text-center text-lg-start mb-lg-5">
+          <h1 class="display-4 text-white mb-4 animated slideInRight">
+            Data Collection
+          </h1>
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb justify-content-center justify-content-lg-start mb-0">
+              <li class="breadcrumb-item">
+                <a class="text-white" href="#">Home</a>
+              </li>
+              <li class="breadcrumb-item">
+                <a class="text-white" href="#">Data Collection</a>
+              </li>
+              <!-- <li class="breadcrumb-item text-white active" aria-current="page">Our Projects</li> -->
+            </ol>
+          </nav>
+        </div>
+        <div class="col-lg-6 align-self-end text-center text-lg-end">
+          <img class="img-fluid" src="img/hero-img.png" alt="" style="max-height: 300px" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Hero End -->
+
+  <!-- Full Screen Search Start -->
+  <div class="modal fade" id="searchModal" tabindex="-1">
+    <div class="modal-dialog modal-fullscreen">
+      <div class="modal-content" style="background: rgba(20, 24, 62, 0.7)">
+        <div class="modal-header border-0">
+          <button type="button" class="btn btn-square bg-white btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body d-flex align-items-center justify-content-center">
+          <div class="input-group" style="max-width: 600px">
+            <input type="text" class="form-control bg-transparent border-light p-3" placeholder="Type search keyword" />
+            <button class="btn btn-light px-4">
+              <i class="bi bi-search"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Full Screen Search End -->
+  <br><br>
+  
+
+  <br><br>
+  
+  
+  </div>
+
+  
+  
+  </div>
+
+  
+  <?php include("includes/footer.php"); ?>
+
+  <!-- Back to Top -->
+  <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top pt-2"><i class="bi bi-arrow-up"></i></a>
+
+  <!-- JavaScript Libraries -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/waypoints/waypoints.min.js"></script>
+  <script src="lib/counterup/counterup.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+  <!-- Template Javascript -->
+  <script src="js/main.js"></script>
+</body>
+
+</html>
+</html>
